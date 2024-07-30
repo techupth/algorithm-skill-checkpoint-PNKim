23a63c3e301f482a01a734d8cfd3c81fbdfd4ca1
PNKim <paljapon.nait@gmail.com>
2024-07-30 10:33:39 +0700

    feat: resolve question-5

--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
-Answer:
+Answer: O(n^2) เนื่องจากเป็นการทำงานโดยใช้ for-loop ซ้อนกัน 2 ชั้น ทำให้จำนวนรอบการทำงานมีเพิ่มมากขึ้น
 
 
 */
