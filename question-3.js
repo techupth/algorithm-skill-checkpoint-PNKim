fd044421460f012da5681a380bc19fc0688c0320
PNKim <paljapon.nait@gmail.com>
2024-07-30 10:28:51 +0700

    feat: resolve question-3

--- question-3.js
+++ question-3.js
@@ -1,20 +1,21 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
+Answer: O(n) เพราะมีการวนลูป for ของ array เพียงครั้งเดียวโดยเริ่มตั้งแต่ index ที่ 0 จนครบทุกตัว
 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
+Answer: O(log n) เนื่องจากการทำงานแบบ binary search จะเพิ่มรอบการทำงานตาม log n 
 
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
+Answer: แบบ binary search จะมีประสิทธิภาพมากกว่าหากว่าเป็น array ที่มีการเรียงลำดับเอาไว้แล้ว
+หาก array ไม่มีการเรียงลำดับก็ควรใช้แบบ linear search มากกว่า
 
 
 */
