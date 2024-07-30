926a5ba08c40d1d97b10c9fab69ce88cad2ec648
PNKim <paljapon.nait@gmail.com>
2024-07-30 10:31:53 +0700

    feat: resolve question-4

--- question-4.js
+++ question-4.js
@@ -1,5 +1,15 @@
 function sortProductsByPrice(products) {
   // Your code here
+  for (let i = 0; i < products.length - 1; i++) {
+    for (let j = i; j < products.length - 1 - i; j++) {
+      if (products[j].price > products[j + 1].price) {
+        let newProduct = products[j + 1];
+        products[j + 1] = products[j];
+        products[j] = newProduct;
+      }
+    }
+  }
+  return products;
 }
 
 // Test case
